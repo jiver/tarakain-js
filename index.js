df25cffd2815c62acff7e0af5b59a6cdac9cf990
christiankyle23 <50711201+christiankyle23@users.noreply.github.com>
2019-05-17 01:47:11 +0800

    edit error command

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ adapter.onTurnError = async (context, error) => {
     //       application insights.
     console.error(`\n [onTurnError]: ${ error }`);
     // Send a message to the user
-    await context.sendActivity(`Oops. Something went wrong!`);
+    await context.sendActivity(`Tangina, wait lang!`);
     // Clear out state
     await conversationState.delete(context);
 };
